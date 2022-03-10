a7cf350b310a0f6f9c8e9ba93ea44150d8a66adc
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 16:20:50 -0500

    se crean constructores de VentaElectronicaId y CanaldePagoId en venta electronica

--- src/main/java/co/com/sofka/challengeDDD/domain/ventaelectronica/ids/VentaElectronicaId.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/ventaelectronica/ids/VentaElectronicaId.java
@@ -1,4 +1,19 @@
 package co.com.sofka.challengeDDD.domain.ventaelectronica.ids;
 
-public class VentaElectronicaId {
+import co.com.sofka.domain.generic.Identity;
+
+public class VentaElectronicaId extends Identity {
+
+    // constructores
+
+    public VentaElectronicaId(String uuid) {
+        super(uuid);
+    }
+
+    public VentaElectronicaId() {
+    }
+
+    public static VentaElectronicaId of(String value){
+        return new VentaElectronicaId(value);
+    }
 }
