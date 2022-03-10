0fd3c4a6a5f5617e8acca0db0b1266cbf2ed47e4
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 15:57:34 -0500

    se implementa PoliticaDeRetiro en valueObjects de venta electronica

--- src/main/java/co/com/sofka/challengeDDD/domain/ventaelectronica/valueobjects/PoliticaDeRetiro.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/ventaelectronica/valueobjects/PoliticaDeRetiro.java
@@ -1,4 +1,35 @@
 package co.com.sofka.challengeDDD.domain.ventaelectronica.valueobjects;
 
-public class PoliticaDeRetiro {
+import co.com.sofka.challengeDDD.domain.ventaelectronica.valueobjects.properties_values.PoliticaDeRetiroValue;
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class PoliticaDeRetiro implements ValueObject<PoliticaDeRetiroValue> {
+
+    // Declaración de variable
+    private final PoliticaDeRetiroValue value;
+
+    public PoliticaDeRetiro(PoliticaDeRetiroValue value) {
+        this.value = value;
+    }
+
+    @Override
+    public PoliticaDeRetiroValue value() {
+        return value;
+    }
+
+    // se implementa el equal y hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PoliticaDeRetiro)) return false;
+        PoliticaDeRetiro that = (PoliticaDeRetiro) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
