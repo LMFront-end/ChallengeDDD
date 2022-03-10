20ff38af5f1b443ecdbdc30efc9f33cd15a1fce0
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 11:39:19 -0500

    se implementa object value TipoProductoJuguete

--- src/main/java/co/com/sofka/challengeDDD/domain/utileria/valueobjects/TipoProductoJuguete.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/utileria/valueobjects/TipoProductoJuguete.java
@@ -1,4 +1,36 @@
 package co.com.sofka.challengeDDD.domain.utileria.valueobjects;
 
-public class TipoProductoJuguete {
+import co.com.sofka.challengeDDD.domain.utileria.valueobjects.properties_values.TipoProductoJugueteValue;
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class TipoProductoJuguete implements ValueObject<TipoProductoJugueteValue> {
+
+    // se crea la variable
+    private final TipoProductoJugueteValue value;
+
+    public TipoProductoJuguete(TipoProductoJugueteValue value) {
+        this.value = value;
+    }
+
+    @Override
+    public TipoProductoJugueteValue value() {
+        return value;
+    }
+
+    // se implementa el equal y hashCode
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TipoProductoJuguete)) return false;
+        TipoProductoJuguete that = (TipoProductoJuguete) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
