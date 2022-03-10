9aaa871ea5d6b544a176d605a5f247e703482a2d
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 16:45:01 -0500

    se completa la entidad Modelo con comportamientos

--- src/main/java/co/com/sofka/challengeDDD/domain/show/entities/Modelo.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/show/entities/Modelo.java
@@ -6,6 +6,8 @@ import co.com.sofka.challengeDDD.generics.Identificacion;
 import co.com.sofka.challengeDDD.generics.Nombre;
 import co.com.sofka.domain.generic.Entity;
 
+import java.util.Objects;
+
 public class Modelo extends Entity<ModeloId> {
 
     // se crean variables para los objectValues
@@ -19,6 +21,26 @@ public class Modelo extends Entity<ModeloId> {
 
         // super --> herencia
         super(modeloId);
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.email = email;
     }
+
+        // métodos de la entidad Modelo
+
+        // cambiarNombre()
+        public void cambiarNombre(Nombre nombre){
+            this.nombre = Objects.requireNonNull(nombre);
+        }
+
+        // cambiarIdentificacion()
+        public void cambiarIdentificacion(Identificacion identificacion){
+            this.identificacion = Objects.requireNonNull(identificacion);
+        }
+
+        // cambiarEmail()
+        public void cambiarEmail(Email email){
+            this.email = Objects.requireNonNull(email);
+        }
 
 }
