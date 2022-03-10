47d88baa3c79e058c136312f045fc6dbea02672b
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 16:01:33 -0500

    se implementa ValorMoneda en valueObjects de venta electronica

--- src/main/java/co/com/sofka/challengeDDD/domain/ventaelectronica/valueobjects/ValorMoneda.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/ventaelectronica/valueobjects/ValorMoneda.java
@@ -1,4 +1,41 @@
 package co.com.sofka.challengeDDD.domain.ventaelectronica.valueobjects;
 
-public class ValorMoneda {
+import co.com.sofka.challengeDDD.domain.ventaelectronica.valueobjects.properties_values.ValorMonedaProperties;
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class ValorMoneda implements ValueObject<ValorMonedaProperties> {
+
+    // Declaración de variable
+    private final Double valorMoneda;
+
+    public ValorMoneda(Double valorMoneda) {
+        this.valorMoneda = valorMoneda;
+    }
+
+    @Override
+    public ValorMonedaProperties value() {
+        return new ValorMonedaProperties() {
+            @Override
+            public Double valorMoneda() {
+                return valorMoneda;
+            }
+        };
+    }
+
+    // se implementa el equal y hashCode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ValorMoneda)) return false;
+        ValorMoneda that = (ValorMoneda) o;
+        return valorMoneda.equals(that.valorMoneda);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(valorMoneda);
+    }
 }
