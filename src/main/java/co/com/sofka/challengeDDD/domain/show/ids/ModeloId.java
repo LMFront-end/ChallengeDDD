010a5db9ebd219aff988cd0a50d6deac9fd549ac
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 01:44:37 -0500

    se añade showId en la carpeta ids

--- src/main/java/co/com/sofka/challengeDDD/domain/show/ids/ModeloId.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/show/ids/ModeloId.java
@@ -1,4 +1,19 @@
 package co.com.sofka.challengeDDD.domain.show.ids;
 
-public class ModeloId {
+import co.com.sofka.domain.generic.Identity;
+
+public class ModeloId extends Identity {
+
+    // constructor
+
+    public ModeloId(String uuid) {
+        super(uuid);
+    }
+
+    public ModeloId() {
+    }
+
+    public static MonitorId of(String value){
+        return new MonitorId(value);
+    }
 }
--- src/main/java/co/com/sofka/challengeDDD/domain/show/ids/ShowId.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/show/ids/ShowId.java
@@ -1,4 +1,19 @@
 package co.com.sofka.challengeDDD.domain.show.ids;
 
-public class ShowId {
+import co.com.sofka.domain.generic.Identity;
+
+public class ShowId extends Identity {
+
+    // constructores
+
+    public ShowId(String uuid) {
+        super(uuid);
+    }
+
+    public ShowId() {
+    }
+
+    public static ShowId of(String value){
+        return new ShowId(value);
+    }
 }
