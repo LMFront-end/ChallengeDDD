83925f3c32f105d7f274a2e95f163a412245e935
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 18:09:33 -0500

    se completa entidad Espacio en utileria

--- src/main/java/co/com/sofka/challengeDDD/domain/utileria/entities/Espacio.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/utileria/entities/Espacio.java
@@ -1,4 +1,37 @@
 package co.com.sofka.challengeDDD.domain.utileria.entities;
 
-public class Espacio {
+import co.com.sofka.challengeDDD.domain.utileria.ids.EspacioID;
+import co.com.sofka.challengeDDD.domain.utileria.valueobjects.EstadoHabitacion;
+import co.com.sofka.challengeDDD.domain.utileria.valueobjects.TipoHabitacion;
+import co.com.sofka.domain.generic.Entity;
+
+import java.util.Objects;
+
+
+public class Espacio extends Entity<EspacioID> {
+
+    // se crean variables para los objectValue
+
+    private TipoHabitacion tipoHabitacion;
+    private EstadoHabitacion estadoHabitacion;
+
+    // se crea constructor para los objectValue
+
+    public Espacio(EspacioID entityId, TipoHabitacion tipoHabitacion, EstadoHabitacion estadoHabitacion) {
+        super(entityId);
+        this.tipoHabitacion = tipoHabitacion;
+        this.estadoHabitacion = estadoHabitacion;
+    }
+
+    // métodos de la entidad Espacio
+
+    // cambiarTipoHabitacion()
+    public void cambiarTipoHabitacion(TipoHabitacion tipoHabitacion) {
+        this.tipoHabitacion = Objects.requireNonNull(tipoHabitacion);
+    }
+
+    // CambiarEstadoHabitacion()
+    public void CambiarEstadoHabitacion(EstadoHabitacion estadoHabitacion){
+        this.estadoHabitacion = Objects.requireNonNull(estadoHabitacion);
+    }
 }
