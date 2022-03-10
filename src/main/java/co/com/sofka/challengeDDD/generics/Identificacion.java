53da5c2733746c3b506186b6e4ba01e1542f0ec7
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 01:07:34 -0500

    Se crea value object identificacion

--- src/main/java/co/com/sofka/challengeDDD/generics/Identificacion.java
+++ src/main/java/co/com/sofka/challengeDDD/generics/Identificacion.java
@@ -1,4 +1,42 @@
 package co.com.sofka.challengeDDD.generics;
 
-public class Identificacion {
+import co.com.sofka.challengeDDD.generics.properties_values.IdentificacionProperties;
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Identificacion implements ValueObject<IdentificacionProperties> {
+
+    // declaración de variables
+    private final String identificacion;
+
+    // constructor
+    public Identificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    @Override
+    public IdentificacionProperties value() {
+        return new IdentificacionProperties(){
+
+            @Override
+            public String identificacion() {
+                return identificacion;
+            }
+        };
+    }
+
+    // implementación de equals y el hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Identificacion)) return false;
+        Identificacion that = (Identificacion) o;
+        return identificacion.equals(that.identificacion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(identificacion);
+    }
 }
