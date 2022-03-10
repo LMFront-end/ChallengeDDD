c63f9b9e49286f254c1ff0a8c8caa70c3deac650
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 12:13:14 -0500

    se implementa ProductoId, UtileriaId

--- src/main/java/co/com/sofka/challengeDDD/domain/utileria/ids/ProductoID.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/utileria/ids/ProductoID.java
@@ -1,4 +1,19 @@
 package co.com.sofka.challengeDDD.domain.utileria.ids;
 
-public class ProductoID {
+import co.com.sofka.domain.generic.Identity;
+
+public class ProductoID extends Identity {
+
+    // Constructores
+
+    public ProductoID(String uuid) {
+        super(uuid);
+    }
+
+    public ProductoID() {
+    }
+
+    public static ProductoID of(String value){
+        return new ProductoID(value);
+    }
 }
