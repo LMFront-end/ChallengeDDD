2a72206d18ee4de8747922bc9fee8c049a0eab01
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 11:34:46 -0500

    se implementa object value TipoHabitacion

--- src/main/java/co/com/sofka/challengeDDD/domain/utileria/valueobjects/TipoHabitacion.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/utileria/valueobjects/TipoHabitacion.java
@@ -1,4 +1,36 @@
 package co.com.sofka.challengeDDD.domain.utileria.valueobjects;
 
-public class TipoHabitacion {
+import co.com.sofka.challengeDDD.domain.utileria.valueobjects.properties_values.TipoHabitacionValue;
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class TipoHabitacion implements ValueObject<TipoHabitacionValue> {
+
+    // se crea la variable
+    private final TipoHabitacionValue value;
+
+    public TipoHabitacion(TipoHabitacionValue value) {
+        this.value = value;
+    }
+
+    @Override
+    public TipoHabitacionValue value() {
+        return value;
+    }
+
+    // se implementa el equal y hashCode
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TipoHabitacion)) return false;
+        TipoHabitacion that = (TipoHabitacion) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
