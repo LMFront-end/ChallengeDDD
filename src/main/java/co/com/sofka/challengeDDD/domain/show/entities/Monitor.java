2f286dddf9f84875068060e0b6f8be5024a0bc0d
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 16:54:09 -0500

    se crea contructor para object value de la entidad Monitor

--- src/main/java/co/com/sofka/challengeDDD/domain/show/entities/Monitor.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/show/entities/Monitor.java
@@ -1,4 +1,23 @@
 package co.com.sofka.challengeDDD.domain.show.entities;
 
-public class Monitor {
+import co.com.sofka.challengeDDD.domain.show.ids.MonitorId;
+import co.com.sofka.challengeDDD.domain.show.valueobjects.AcuerdoDeConfidencialidad;
+import co.com.sofka.challengeDDD.generics.Identificacion;
+import co.com.sofka.challengeDDD.generics.Nombre;
+import co.com.sofka.domain.generic.Entity;
+
+public class Monitor extends Entity<MonitorId> {
+
+    // se crean variables para los ovjectValue
+
+    private Nombre nombre;
+    private Identificacion identificacion;
+    private AcuerdoDeConfidencialidad acuerdoDeConfidencialidad;
+
+    // se crea constructor para los objectValue
+    public Monitor(MonitorId monitorId, Nombre nombre, Identificacion identificacion, AcuerdoDeConfidencialidad acuerdoDeConfidencialidad){
+
+        // Super --> Herencia
+        super(monitorId);
+    }
 }
