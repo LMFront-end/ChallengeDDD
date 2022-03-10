025a67bdba0b8c258387b6c99b034a7dfe608ea9
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 01:27:36 -0500

    Se implementa acuerdo de confidencialidad y paginas de transmision

---
+++ src/main/java/co/com/sofka/challengeDDD/domain/show/valueobjects/PaginasTransmision.java
@@ -0,0 +1,37 @@
+package co.com.sofka.challengeDDD.domain.show.valueobjects;
+
+import co.com.sofka.challengeDDD.domain.show.valueobjects.properties_values.PaginaDeTransmisionValue;
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class PaginasTransmision implements ValueObject<PaginaDeTransmisionValue> {
+
+    // se crean variables
+    private final PaginaDeTransmisionValue value;
+
+    // contructor
+    public PaginasTransmision(PaginaDeTransmisionValue value) {
+        this.value = value;
+    }
+
+    @Override
+    public PaginaDeTransmisionValue value() {
+        return value;
+    }
+
+    // se implementa el equal y hashCode
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PaginasTransmision)) return false;
+        PaginasTransmision that = (PaginasTransmision) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/main/java/co/com/sofka/challengeDDD/domain/show/valueobjects/properties_values/PaginaDeTransmisionValue.java
@@ -0,0 +1,14 @@
+package co.com.sofka.challengeDDD.domain.show.valueobjects.properties_values;
+
+public enum PaginaDeTransmisionValue {
+
+    // valores de página de transmisión
+
+    LIVEJAZMIN,
+    CANARIANLIVES,
+    CAM4,
+    BONGOCAMS,
+    CHATURBATE,
+    CAMSODA
+
+}
