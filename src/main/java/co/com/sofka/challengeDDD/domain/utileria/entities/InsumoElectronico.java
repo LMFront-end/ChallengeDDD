b72c0498b783722c85563106588d2183c215ff5b
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 18:19:03 -0500

    se completa entidad InsumoElectronico en utileria

--- src/main/java/co/com/sofka/challengeDDD/domain/utileria/entities/InsumoElectronico.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/utileria/entities/InsumoElectronico.java
@@ -1,4 +1,37 @@
 package co.com.sofka.challengeDDD.domain.utileria.entities;
 
-public class InsumoElectronico {
+import co.com.sofka.challengeDDD.domain.utileria.ids.InsumoID;
+import co.com.sofka.challengeDDD.domain.utileria.valueobjects.EstadoInsumo;
+import co.com.sofka.challengeDDD.domain.utileria.valueobjects.TipoDeInsumo;
+import co.com.sofka.domain.generic.Entity;
+
+import java.util.Objects;
+
+public class InsumoElectronico extends Entity<InsumoID> {
+
+    // se crean variables para los objectValue
+
+    private TipoDeInsumo tipoDeInsumo;
+    private EstadoInsumo estadoInsumo;
+
+    // se crea constructor para los objectValue
+
+
+    public InsumoElectronico(InsumoID entityId, TipoDeInsumo tipoDeInsumo, EstadoInsumo estadoInsumo) {
+        super(entityId);
+        this.tipoDeInsumo = tipoDeInsumo;
+        this.estadoInsumo = estadoInsumo;
+    }
+
+    // métodos de la entidad InsumoElectronico
+
+    // cambiarTipoInsumo()
+    public void cambiarTipoInsumo(TipoDeInsumo tipoDeInsumo){
+        this.tipoDeInsumo = Objects.requireNonNull(tipoDeInsumo);
+    }
+
+    // cambiarEstadoInsumo()
+    public void cambiarEstadoInsumo(EstadoInsumo estadoInsumo){
+        this.estadoInsumo = Objects.requireNonNull(estadoInsumo);
+    }
 }
