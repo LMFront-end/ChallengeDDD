133e0f497e6dbd1a7594ce0d3060a8859077c292
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 16:26:23 -0500

    se crean constructor de TransaccionId en venta electronica

--- src/main/java/co/com/sofka/challengeDDD/domain/ventaelectronica/ids/TransaccionId.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/ventaelectronica/ids/TransaccionId.java
@@ -1,4 +1,19 @@
 package co.com.sofka.challengeDDD.domain.ventaelectronica.ids;
 
-public class TransaccionId {
+import co.com.sofka.domain.generic.Identity;
+
+public class TransaccionId extends Identity {
+
+    // constructores
+
+    public TransaccionId(String uuid) {
+        super(uuid);
+    }
+
+    public TransaccionId() {
+    }
+
+    public static TransaccionId of(String value){
+        return new TransaccionId(value);
+    }
 }
