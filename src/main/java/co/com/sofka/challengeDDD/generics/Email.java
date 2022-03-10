1fc48a599fbd4b37b36cd7f6740dc86e507b8b81
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 00:59:27 -0500

    Se crea value object Email

--- src/main/java/co/com/sofka/challengeDDD/generics/Email.java
+++ src/main/java/co/com/sofka/challengeDDD/generics/Email.java
@@ -1,4 +1,43 @@
 package co.com.sofka.challengeDDD.generics;
 
-public class Email implements {
+import co.com.sofka.challengeDDD.generics.properties_values.EmailProperties;
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Email implements ValueObject<EmailProperties> {
+
+    // declaración de variables
+    private final String correo;
+
+    // constructor email
+    public Email(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public EmailProperties value() {
+        return new EmailProperties() {
+
+            @Override
+            public String correo() {
+                return correo;
+            }
+        };
+    }
+
+    // implementación de equals y el hashCode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Email)) return false;
+        Email email = (Email) o;
+        return correo.equals(email.correo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(correo);
+    }
 }
