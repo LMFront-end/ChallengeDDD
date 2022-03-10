2264d4db8f89d4e9b974205fc21317631e682363
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 11:30:43 -0500

    se implementa object value EstadoProducto

--- src/main/java/co/com/sofka/challengeDDD/domain/utileria/valueobjects/EstadoProducto.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/utileria/valueobjects/EstadoProducto.java
@@ -1,4 +1,36 @@
 package co.com.sofka.challengeDDD.domain.utileria.valueobjects;
 
-public class EstadoProducto {
+import co.com.sofka.challengeDDD.domain.utileria.valueobjects.properties_values.EstadoProductoValue;
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class EstadoProducto implements ValueObject<EstadoProductoValue> {
+
+    // se crea la variable
+    private final EstadoProductoValue value;
+
+    public EstadoProducto(EstadoProductoValue value) {
+        this.value = value;
+    }
+
+    @Override
+    public EstadoProductoValue value() {
+        return value;
+    }
+
+    // se implementa el equal y hashCode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EstadoProducto)) return false;
+        EstadoProducto that = (EstadoProducto) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
