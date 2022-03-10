6db06e939a8e561c43a6e0c8723a089ec0e17889
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 12:16:11 -0500

    se implementa InsumoId

--- src/main/java/co/com/sofka/challengeDDD/domain/utileria/ids/InsumoID.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/utileria/ids/InsumoID.java
@@ -1,4 +1,19 @@
 package co.com.sofka.challengeDDD.domain.utileria.ids;
 
-public class InsumoID {
+import co.com.sofka.domain.generic.Identity;
+
+public class InsumoID extends Identity {
+
+    // constructores
+
+    public InsumoID(String uuid) {
+        super(uuid);
+    }
+
+    public InsumoID() {
+    }
+
+    public static InsumoID of(String value){
+        return new InsumoID(value);
+    }
 }
