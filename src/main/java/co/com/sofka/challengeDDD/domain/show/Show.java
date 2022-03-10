d7c2bd67367a208b9f4059c2a424f2b28066dc29
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 00:36:59 -0500

    Se crea estructura de carpetas de los agregados

---
+++ src/main/java/co/com/sofka/challengeDDD/domain/show/Show.java
@@ -0,0 +1,4 @@
+package co.com.sofka.challengeDDD.domain.show;
+
+public class Show {
+}
---
+++ src/main/java/co/com/sofka/challengeDDD/domain/ventaelectronica/VentaElectronica.java
@@ -0,0 +1,4 @@
+package co.com.sofka.challengeDDD.domain.ventaelectronica;
+
+public class VentaElectronica {
+}
