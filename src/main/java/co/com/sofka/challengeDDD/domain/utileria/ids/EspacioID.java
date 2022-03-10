aa178b38b8ca3b5f268245aef788a5fb33883c7b
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 12:14:43 -0500

    se implementa EspacioId

--- src/main/java/co/com/sofka/challengeDDD/domain/utileria/ids/EspacioID.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/utileria/ids/EspacioID.java
@@ -1,4 +1,19 @@
 package co.com.sofka.challengeDDD.domain.utileria.ids;
 
-public class EspacioID {
+import co.com.sofka.domain.generic.Identity;
+
+public class EspacioID extends Identity {
+
+    // constructores
+
+    public EspacioID(String uuid) {
+        super(uuid);
+    }
+
+    public EspacioID() {
+    }
+
+    public static EspacioID of(String value){
+        return new EspacioID(value);
+    }
 }
