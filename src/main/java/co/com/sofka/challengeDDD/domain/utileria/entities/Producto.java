aa9e7bc45903cd42e7f3806a1b1be5d7ede0342d
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 17:54:34 -0500

    se crea constructor para objectValue en utileria

--- src/main/java/co/com/sofka/challengeDDD/domain/utileria/entities/Producto.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/utileria/entities/Producto.java
@@ -1,4 +1,23 @@
 package co.com.sofka.challengeDDD.domain.utileria.entities;
 
-public class Producto {
+import co.com.sofka.challengeDDD.domain.utileria.ids.ProductoID;
+import co.com.sofka.challengeDDD.domain.utileria.valueobjects.EstadoProducto;
+import co.com.sofka.challengeDDD.domain.utileria.valueobjects.TipoProductoDisfraz;
+import co.com.sofka.challengeDDD.domain.utileria.valueobjects.TipoProductoJuguete;
+import co.com.sofka.domain.generic.Entity;
+
+public class Producto extends Entity<ProductoID> {
+
+    // se crean variables para los objectValue
+
+    private TipoProductoJuguete tipoProductoJuguete;
+    private TipoProductoDisfraz tipoProductoDisfraz;
+    private EstadoProducto estadoProducto;
+
+    // se crea constructor para los objectValue
+    public Producto(ProductoID productoId, TipoProductoJuguete tipoProductoJuguete, TipoProductoDisfraz tipoProductoDisfraz ){
+
+        // Super --> Herencia
+        super(productoId);
+    }
 }
