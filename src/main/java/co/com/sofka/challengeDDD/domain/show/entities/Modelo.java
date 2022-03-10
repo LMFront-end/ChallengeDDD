ab98c13c081d5096240b5313a5a37f46c5f3316a
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 16:37:31 -0500

    se crea entidad Modelo

--- src/main/java/co/com/sofka/challengeDDD/domain/show/entities/Modelo.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/show/entities/Modelo.java
@@ -1,4 +1,24 @@
 package co.com.sofka.challengeDDD.domain.show.entities;
 
-public class Modelo {
+import co.com.sofka.challengeDDD.domain.show.ids.ModeloId;
+import co.com.sofka.challengeDDD.generics.Email;
+import co.com.sofka.challengeDDD.generics.Identificacion;
+import co.com.sofka.challengeDDD.generics.Nombre;
+import co.com.sofka.domain.generic.Entity;
+
+public class Modelo extends Entity<ModeloId> {
+
+    // se crean variables para los objectValues
+
+    private Nombre nombre;
+    private Identificacion identificacion;
+    private Email email;
+
+    // se crea constructor para los objectValue
+    public Modelo(ModeloId modeloId, Nombre nombre, Identificacion identificacion, Email email){
+
+        // super --> herencia
+        super(modeloId);
+    }
+
 }
