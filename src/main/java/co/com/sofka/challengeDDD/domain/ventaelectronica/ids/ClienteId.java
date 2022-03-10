2b42be8e22a065f7abea587649d5425abeb5bd23
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 16:27:48 -0500

    se crean constructor de ClienteId en venta electronica

--- src/main/java/co/com/sofka/challengeDDD/domain/ventaelectronica/ids/ClienteId.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/ventaelectronica/ids/ClienteId.java
@@ -1,4 +1,19 @@
 package co.com.sofka.challengeDDD.domain.ventaelectronica.ids;
 
-public class ClienteId {
+import co.com.sofka.domain.generic.Identity;
+
+public class ClienteId extends Identity {
+
+    // constructores
+
+    public ClienteId(String uuid) {
+        super(uuid);
+    }
+
+    public ClienteId() {
+    }
+
+    public static ClienteId of(String value){
+        return new ClienteId(value);
+    }
 }
