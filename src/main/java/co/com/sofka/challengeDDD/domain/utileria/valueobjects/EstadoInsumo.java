8d6478e157565fc354baeb03040144ed999a3285
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 11:28:39 -0500

    se implementa object value EstadoInsumo

--- src/main/java/co/com/sofka/challengeDDD/domain/utileria/valueobjects/EstadoInsumo.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/utileria/valueobjects/EstadoInsumo.java
@@ -1,4 +1,37 @@
 package co.com.sofka.challengeDDD.domain.utileria.valueobjects;
 
-public class EstadoInsumo {
+import co.com.sofka.challengeDDD.domain.utileria.valueobjects.properties_values.EstadoInsumoValue;
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class EstadoInsumo implements ValueObject<EstadoInsumoValue> {
+
+    // se crea la variable
+    private final EstadoInsumoValue value;
+
+    // se crea el constructor
+    public EstadoInsumo(EstadoInsumoValue value) {
+        this.value = value;
+    }
+
+    @Override
+    public EstadoInsumoValue value() {
+        return value;
+    }
+
+    // se implementa el equal y hashCode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EstadoInsumo)) return false;
+        EstadoInsumo that = (EstadoInsumo) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
