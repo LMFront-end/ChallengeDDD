7ee6d3b4b18c0336a80b22fd28ed3f8e1e81f7e8
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-11 14:34:02 -0500

    se implemente CrearTransaccionUseCase en ventaElectronica

--- src/main/java/co/com/sofka/challengeDDD/aplications/usecase/ventaelectronica/command/CrearTransaccionUseCase.java
+++ src/main/java/co/com/sofka/challengeDDD/aplications/usecase/ventaelectronica/command/CrearTransaccionUseCase.java
@@ -1,4 +1,21 @@
 package co.com.sofka.challengeDDD.aplications.usecase.ventaelectronica.command;
 
-public class CrearTransaccionUseCase {
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.challengeDDD.domain.ventaelectronica.VentaElectronica;
+import co.com.sofka.challengeDDD.domain.ventaelectronica.commands.CrearTransaccion;
+
+public class CrearTransaccionUseCase extends UseCase<RequestCommand<CrearTransaccion>, ResponseEvents> {
+
+    // Se implementa el método
+
+    @Override
+    public void executeUseCase(RequestCommand<CrearTransaccion> input) {
+
+        var command = input.getCommand();
+        // se crea una nueva instancia
+        var ventaElectronica = new VentaElectronica(command.getVentaElectronicaId(), command.getShowId());
+        emit().onResponse(new ResponseEvents(ventaElectronica.getUncommittedChanges()));
+    }
 }
