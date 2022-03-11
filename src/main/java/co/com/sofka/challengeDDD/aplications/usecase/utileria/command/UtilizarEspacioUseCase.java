5c1e1314daf74d77a95cf0b47ce54202b8ab5027
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-11 13:57:35 -0500

    se crea utilizarEspacioUseCase en utileria

--- src/main/java/co/com/sofka/challengeDDD/aplications/usecase/utileria/command/UtilizarEspacioUseCase.java
+++ src/main/java/co/com/sofka/challengeDDD/aplications/usecase/utileria/command/UtilizarEspacioUseCase.java
@@ -1,4 +1,23 @@
 package co.com.sofka.challengeDDD.aplications.usecase.utileria.command;
 
-public class UtilizarEspacioUseCase {
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.challengeDDD.domain.utileria.Utileria;
+import co.com.sofka.challengeDDD.domain.utileria.commands.UtilizarEspacio;
+
+public class UtilizarEspacioUseCase extends UseCase<RequestCommand<UtilizarEspacio>, ResponseEvents> {
+
+    // implementar método
+    @Override
+    public void executeUseCase(RequestCommand<UtilizarEspacio> input) {
+
+        // Se recoge la instancia
+        // se debe pasar la referencia de showId
+        var command = input.getCommand();
+
+        var utileria = Utileria.from(command.getUtileriaID(), command.getShowId(), retrieveEvents());
+        utileria.utilizarEspacio(command.getEspacioId(), command.getTipoHabitacion(), command.getEstadoHabitacion());
+        emit().onResponse(new ResponseEvents(utileria.getUncommittedChanges()));
+    }
 }
--- src/main/java/co/com/sofka/challengeDDD/domain/utileria/commands/UtilizarEspacio.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/utileria/commands/UtilizarEspacio.java
@@ -5,12 +5,13 @@ import co.com.sofka.challengeDDD.domain.utileria.ids.EspacioID;
 import co.com.sofka.challengeDDD.domain.utileria.ids.UtileriaID;
 import co.com.sofka.challengeDDD.domain.utileria.valueobjects.EstadoHabitacion;
 import co.com.sofka.challengeDDD.domain.utileria.valueobjects.TipoHabitacion;
+import co.com.sofka.domain.generic.Command;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class UtilizarEspacio {
+public class UtilizarEspacio extends Command {
 
     private final ShowId showId;
     private final UtileriaID utileriaID;
