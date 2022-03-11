b2c18fdac2831ad97f2737b8cf7fcb1dbee057ed
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-11 01:56:00 -0500

    se implementa useCase de AsignarModelo

--- src/main/java/co/com/sofka/challengeDDD/usecase/show/command/AsignarModeloUseCase.java
+++ src/main/java/co/com/sofka/challengeDDD/usecase/show/command/AsignarModeloUseCase.java
@@ -1,4 +1,21 @@
 package co.com.sofka.challengeDDD.usecase.show.command;
 
-public class AsignarModeloUseCase {
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.challengeDDD.domain.show.Show;
+import co.com.sofka.challengeDDD.domain.show.commands.AsignarModelo;
+
+public class AsignarModeloUseCase extends UseCase<RequestCommand<AsignarModelo>, ResponseEvents> {
+
+    // se implementa el método
+    @Override
+    public void executeUseCase(RequestCommand<AsignarModelo> input) {
+
+        var command =  input.getCommand();
+        // se recoge la instancia
+        var show = Show.from(command.getShowId(), retrieveEvents());
+        show.asignarModelo(command.getModeloId(), command.getNombre(), command.getIdentificacion(), command.getEmail());
+        emit().onResponse(new ResponseEvents(show.getUncommittedChanges()));
+    }
 }
