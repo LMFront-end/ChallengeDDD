c30f4c4e567cf7bf17e8a6d2da1ad17735e8335e
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-11 02:03:11 -0500

    se implementa useCase de cambiarMonitor

--- src/main/java/co/com/sofka/challengeDDD/usecase/show/command/CambiarMonitorUseCase.java
+++ src/main/java/co/com/sofka/challengeDDD/usecase/show/command/CambiarMonitorUseCase.java
@@ -1,4 +1,21 @@
 package co.com.sofka.challengeDDD.usecase.show.command;
 
-public class CambiarMonitorUseCase {
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.challengeDDD.domain.show.Show;
+import co.com.sofka.challengeDDD.domain.show.commands.CambiarMonitor;
+
+public class CambiarMonitorUseCase extends UseCase<RequestCommand<CambiarMonitor>, ResponseEvents> {
+
+    // se implementa el método
+    @Override
+    public void executeUseCase(RequestCommand<CambiarMonitor> input) {
+
+        var command =  input.getCommand();
+        // se recoge la instancia
+        var show = Show.from(command.getShowId(), retrieveEvents());
+        show.cambiarMonitor(command.getMonitorId(), command.getNombre(), command.getIdentificacion(), command.getAcuerdoDeConfidencialidad());
+        emit().onResponse(new ResponseEvents(show.getUncommittedChanges()));
+    }
 }
