64efd55059caf7e51d309b63dce4eba52d9e77d3
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-11 14:32:19 -0500

    se implemente CrearClienteUseCase en ventaElectronica

--- src/main/java/co/com/sofka/challengeDDD/aplications/usecase/ventaelectronica/command/CrearClienteUseCase.java
+++ src/main/java/co/com/sofka/challengeDDD/aplications/usecase/ventaelectronica/command/CrearClienteUseCase.java
@@ -1,4 +1,21 @@
 package co.com.sofka.challengeDDD.aplications.usecase.ventaelectronica.command;
 
-public class CrearClienteUseCase {
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.challengeDDD.domain.ventaelectronica.VentaElectronica;
+import co.com.sofka.challengeDDD.domain.ventaelectronica.commands.CrearCliente;
+
+public class CrearClienteUseCase extends UseCase<RequestCommand<CrearCliente>, ResponseEvents> {
+
+    // Se implementa el método
+
+    @Override
+    public void executeUseCase(RequestCommand<CrearCliente> input) {
+
+        var command = input.getCommand();
+        // se crea una nueva instancia
+        var ventaElectronica = new VentaElectronica(command.getVentaElectronicaId(), command.getShowId());
+        emit().onResponse(new ResponseEvents(ventaElectronica.getUncommittedChanges()));
+    }
 }
