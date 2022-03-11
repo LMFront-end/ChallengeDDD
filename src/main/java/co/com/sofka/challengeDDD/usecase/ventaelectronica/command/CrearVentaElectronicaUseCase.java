50af85b8966f6f92eb12ae15353486cb2e75588e
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-11 01:36:12 -0500

    se implementa CrearVentaElectronicaUseCase en usecase ventaelectronica

--- src/main/java/co/com/sofka/challengeDDD/usecase/ventaelectronica/command/CrearVentaElectronicaUseCase.java
+++ src/main/java/co/com/sofka/challengeDDD/usecase/ventaelectronica/command/CrearVentaElectronicaUseCase.java
@@ -1,4 +1,19 @@
 package co.com.sofka.challengeDDD.usecase.ventaelectronica.command;
 
-public class CrearVentaElectronicaUseCase extends{
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.challengeDDD.domain.ventaelectronica.VentaElectronica;
+import co.com.sofka.challengeDDD.domain.ventaelectronica.commands.CrearVentaElectronica;
+
+public class CrearVentaElectronicaUseCase extends UseCase<RequestCommand<CrearVentaElectronica>, ResponseEvents> {
+
+    // se implementa el método
+    @Override
+    public void executeUseCase(RequestCommand<CrearVentaElectronica> input) {
+
+        var command = input.getCommand();
+        var ventaElectronica = new VentaElectronica(command.getVentaElectronicaId(), command.getShowId());
+        emit().onResponse(new ResponseEvents(ventaElectronica.getUncommittedChanges()));
+    }
 }
