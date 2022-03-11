7f2c51105eb486908c2fd70bd09dba988aa59e05
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-11 00:59:32 -0500

    Se crean metodos en VentaElectronicaChange en el [AR] VentaElectronica

--- src/main/java/co/com/sofka/challengeDDD/domain/ventaelectronica/VentaElectronicaChange.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/ventaelectronica/VentaElectronicaChange.java
@@ -1,10 +1,33 @@
 package co.com.sofka.challengeDDD.domain.ventaelectronica;
 
+import co.com.sofka.challengeDDD.domain.utileria.events.UtileriaCreada;
+import co.com.sofka.challengeDDD.domain.ventaelectronica.events.VentaElectronicaCreada;
 import co.com.sofka.domain.generic.EventChange;
 
 public class VentaElectronicaChange extends EventChange {
 
     public VentaElectronicaChange(VentaElectronica ventaElectronica){
+
+        apply((VentaElectronicaCreada event) -> {
+            ventaElectronica.ventaElectronicaId = event.getVentaElectronicaId();
+            ventaElectronica.showId = event.getShowId();
+        });
+
+        // Métodos
+
+        // crearTransaccion()
+
+        // modificarTransaccion()
+
+        // crearCliente()
+
+        // modificarCliente()
+
+        // crearCanalDePago()
+
+        // modificarCanalDePago()
+
+        
     }
 
 }
