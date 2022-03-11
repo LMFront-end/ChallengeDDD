c03aed723a874a98480638b8712258e3bdf823cc
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-11 01:57:31 -0500

    se implementa useCase de AsignarMonitor

--- src/main/java/co/com/sofka/challengeDDD/usecase/show/command/AsignarMonitorUseCase.java
+++ src/main/java/co/com/sofka/challengeDDD/usecase/show/command/AsignarMonitorUseCase.java
@@ -1,4 +1,21 @@
 package co.com.sofka.challengeDDD.usecase.show.command;
 
-public class AsignarMonitorUseCase {
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.challengeDDD.domain.show.Show;
+import co.com.sofka.challengeDDD.domain.show.commands.AsignarMonitor;
+
+public class AsignarMonitorUseCase extends UseCase<RequestCommand<AsignarMonitor>, ResponseEvents> {
+
+    // se implementa el método
+    @Override
+    public void executeUseCase(RequestCommand<AsignarMonitor> input) {
+
+        var command =  input.getCommand();
+        // se recoge la instancia
+        var show = Show.from(command.getShowId(), retrieveEvents());
+        show.asignarMonitor(command.getMonitorId(), command.getNombre(), command.getIdentificacion(), command.getAcuerdoDeConfidencialidad());
+        emit().onResponse(new ResponseEvents(show.getUncommittedChanges()));
+    }
 }
