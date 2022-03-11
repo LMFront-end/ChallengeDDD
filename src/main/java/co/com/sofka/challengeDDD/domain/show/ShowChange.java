4714de8e269e2ed48dafcdf16d05ae2a638cbb64
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-10 22:39:05 -0500

    se completan los metodos en ShowChange en el agregado Show

--- src/main/java/co/com/sofka/challengeDDD/domain/show/ShowChange.java
+++ src/main/java/co/com/sofka/challengeDDD/domain/show/ShowChange.java
@@ -1,8 +1,9 @@
 package co.com.sofka.challengeDDD.domain.show;
 
+import co.com.sofka.challengeDDD.domain.show.entities.CanalDeTransmision;
 import co.com.sofka.challengeDDD.domain.show.entities.Modelo;
-import co.com.sofka.challengeDDD.domain.show.events.ModeloAsignada;
-import co.com.sofka.challengeDDD.domain.show.events.ShowCreado;
+import co.com.sofka.challengeDDD.domain.show.entities.Monitor;
+import co.com.sofka.challengeDDD.domain.show.events.*;
 import co.com.sofka.domain.generic.EventChange;
 
 public class ShowChange extends EventChange {
@@ -26,13 +27,43 @@ public class ShowChange extends EventChange {
         });
 
         // cambiarModelo()
-
+        apply((ModeloCambiada event) ->{
+            show.modelo.cambiarNombre(event.getNombre());
+            show.modelo.cambiarEmail(event.getEmail());
+            show.modelo.cambiarIdentificacion(event.getIdentificacion());
+        });
 
         // asignarMonitor()
+        apply((MonitorAsignado event) -> {
+            show.monitor = new Monitor(
+                    event.getMonitorId(),
+                    event.getNombre(),
+                    event.getIdentificacion(),
+                    event.getAcuerdoDeConfidencialidad()
+            );
+        });
+
         // cambiarMonitor()
+        apply((MonitorCambiado event) ->{
+            show.monitor.cambiarNombre(event.getNombre());
+            show.monitor.cambiarIdentificacion(event.getIdentificacion());
+            show.monitor.cambiarAcuerdoDeConfidencialidad(event.getAcuerdoDeConfidencialidad());
+        });
+
         // asignarCanalDeTransmision()
+        apply((CanalDeTransmisionAsignado event) -> {
+            show.canalDeTransmision = new CanalDeTransmision(
+                    event.getCanalDeTransmisionId(),
+                    event.getAcuerdoDePago(),
+                    event.getPaginasDeTransmision()
+            );
+        });
+
         // cambiarCanalDeTransmision()
+        apply((CanalDeTransmisionCambiado event) ->{
+            show.canalDeTransmision.cambiarPaginasDeTransmision(event.getPaginasDeTransmision());
+            show.canalDeTransmision.cambiarAcuerdoDePago(event.getAcuerdoDePago());
+        });
     }
-
-
+    
 }
