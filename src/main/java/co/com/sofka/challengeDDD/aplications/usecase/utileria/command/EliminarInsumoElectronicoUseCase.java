d2dd78ead8d5174ab1054f7c22530e3c832fdb49
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-11 13:51:29 -0500

    se crean EliminarProductoUseCase y EliminarInsumoElectronicoUseCase en utileria

--- src/main/java/co/com/sofka/challengeDDD/aplications/usecase/utileria/command/EliminarInsumoElectronicoUseCase.java
+++ src/main/java/co/com/sofka/challengeDDD/aplications/usecase/utileria/command/EliminarInsumoElectronicoUseCase.java
@@ -1,4 +1,23 @@
 package co.com.sofka.challengeDDD.aplications.usecase.utileria.command;
 
-public class EliminarInsumoElectronicoUseCase {
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.challengeDDD.domain.utileria.Utileria;
+import co.com.sofka.challengeDDD.domain.utileria.commands.EliminarInsumoElectronico;
+
+public class EliminarInsumoElectronicoUseCase extends UseCase<RequestCommand<EliminarInsumoElectronico>, ResponseEvents> {
+
+    // Se implementa el método
+    @Override
+    public void executeUseCase(RequestCommand<EliminarInsumoElectronico> input) {
+
+
+        var command =  input.getCommand();
+        // Se recoge la instancia
+        // se debe pasar la referencia de showId
+        var utileria = Utileria.from(command.getUtileriaID(), command.getShowId(), retrieveEvents());
+        utileria.eliminarInsumoElectronico(command.getInsumoID());
+        emit().onResponse(new ResponseEvents(utileria.getUncommittedChanges()));
+    }
 }
--- src/main/java/co/com/sofka/challengeDDD/aplications/usecase/utileria/command/EliminarProductoUseCase.java
+++ src/main/java/co/com/sofka/challengeDDD/aplications/usecase/utileria/command/EliminarProductoUseCase.java
@@ -1,4 +1,26 @@
 package co.com.sofka.challengeDDD.aplications.usecase.utileria.command;
 
-public class EliminarProductoUseCase {
+
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.challengeDDD.domain.utileria.Utileria;
+import co.com.sofka.challengeDDD.domain.utileria.commands.EliminarProducto;
+
+public class EliminarProductoUseCase extends UseCase<RequestCommand<EliminarProducto>, ResponseEvents> {
+
+    // se implementa el método
+    @Override
+    public void executeUseCase(RequestCommand<EliminarProducto> input) {
+
+        // se implementa el método
+        var command =  input.getCommand();
+        // Se recoge la instancia
+        // se debe pasar la referencia de showId
+
+        var utileria = Utileria.from(command.getUtileriaID(), command.getShowId(), retrieveEvents());
+        utileria.eliminarProducto(command.getProductoID());
+        emit().onResponse(new ResponseEvents(utileria.getUncommittedChanges()));
+
+    }
 }
