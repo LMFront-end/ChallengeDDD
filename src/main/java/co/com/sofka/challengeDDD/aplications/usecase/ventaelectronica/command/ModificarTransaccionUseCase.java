2bc5b7f536b8980a8b7ae0571fa1800e2c8d411f
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-11 14:45:27 -0500

    se implementa ModificarTransaccionUseCase en ventaElectronica

--- src/main/java/co/com/sofka/challengeDDD/aplications/usecase/ventaelectronica/command/ModificarTransaccionUseCase.java
+++ src/main/java/co/com/sofka/challengeDDD/aplications/usecase/ventaelectronica/command/ModificarTransaccionUseCase.java
@@ -4,19 +4,18 @@ import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
 import co.com.sofka.challengeDDD.domain.ventaelectronica.VentaElectronica;
-import co.com.sofka.challengeDDD.domain.ventaelectronica.commands.ModificarCanalDePago;
 import co.com.sofka.challengeDDD.domain.ventaelectronica.commands.ModificarTransaccion;
 
 public class ModificarTransaccionUseCase extends UseCase<RequestCommand<ModificarTransaccion>, ResponseEvents> {
 
     // se implementa el método
     @Override
-    public void executeUseCase(RequestCommand<ModificarCanalDePago> input) {
+    public void executeUseCase(RequestCommand<ModificarTransaccion> input) {
 
         var command =  input.getCommand();
         // se recoge la instancia
         var ventaElectronica = VentaElectronica.from(command.getVentaElectronicaId(), command.getShowId(), retrieveEvents());
-        ventaElectronica.modificarCanalDePago(command.getCanalDePagoId(), command.getCanalPagoProveedor(), command.getPoliticaDeRetiro());
+        ventaElectronica.modificarTransaccion(command.getTransaccionId(), command.getValorMoneda(), command.getFecha());
         emit().onResponse(new ResponseEvents(ventaElectronica.getUncommittedChanges()));
 
     }
