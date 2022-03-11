87567aee2fbc77789619f9b0018765f2a2d93a08
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-11 14:42:30 -0500

    se implementa ModificarClienteUseCase en ventaElectronica

--- src/main/java/co/com/sofka/challengeDDD/aplications/usecase/ventaelectronica/command/ModificarClienteUseCase.java
+++ src/main/java/co/com/sofka/challengeDDD/aplications/usecase/ventaelectronica/command/ModificarClienteUseCase.java
@@ -1,4 +1,22 @@
 package co.com.sofka.challengeDDD.aplications.usecase.ventaelectronica.command;
 
-public class ModificarClienteUseCase {
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.challengeDDD.domain.ventaelectronica.VentaElectronica;
+import co.com.sofka.challengeDDD.domain.ventaelectronica.commands.ModificarCliente;
+
+public class ModificarClienteUseCase extends UseCase<RequestCommand<ModificarCliente>, ResponseEvents> {
+
+    // se implementa el método
+    @Override
+    public void executeUseCase(RequestCommand<ModificarCliente> input) {
+
+        var command =  input.getCommand();
+        // se recoge la instancia
+        var ventaElectronica = VentaElectronica.from(command.getVentaElectronicaId(), command.getShowId(), retrieveEvents());
+        ventaElectronica.modificarCliente(command.getClienteId(), command.getNombre(), command.getIdentificacion(), command.getEmail());
+        emit().onResponse(new ResponseEvents(ventaElectronica.getUncommittedChanges()));
+
+    }
 }
--- src/main/java/co/com/sofka/challengeDDD/aplications/usecase/ventaelectronica/command/ModificarTransaccionUseCase.java
+++ src/main/java/co/com/sofka/challengeDDD/aplications/usecase/ventaelectronica/command/ModificarTransaccionUseCase.java
@@ -1,4 +1,23 @@
 package co.com.sofka.challengeDDD.aplications.usecase.ventaelectronica.command;
 
-public class ModificarTransaccionUseCase {
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofka.challengeDDD.domain.ventaelectronica.VentaElectronica;
+import co.com.sofka.challengeDDD.domain.ventaelectronica.commands.ModificarCanalDePago;
+import co.com.sofka.challengeDDD.domain.ventaelectronica.commands.ModificarTransaccion;
+
+public class ModificarTransaccionUseCase extends UseCase<RequestCommand<ModificarTransaccion>, ResponseEvents> {
+
+    // se implementa el método
+    @Override
+    public void executeUseCase(RequestCommand<ModificarCanalDePago> input) {
+
+        var command =  input.getCommand();
+        // se recoge la instancia
+        var ventaElectronica = VentaElectronica.from(command.getVentaElectronicaId(), command.getShowId(), retrieveEvents());
+        ventaElectronica.modificarCanalDePago(command.getCanalDePagoId(), command.getCanalPagoProveedor(), command.getPoliticaDeRetiro());
+        emit().onResponse(new ResponseEvents(ventaElectronica.getUncommittedChanges()));
+
+    }
 }
